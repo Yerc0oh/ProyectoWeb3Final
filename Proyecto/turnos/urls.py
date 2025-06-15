9c270106a9e1e6587beabd1add3433177939c440
Maya <mayaoquendo24@gmail.com>
2025-06-15 11:19:51 -0400

    Ponemos las urls de las views

--- Proyecto/turnos/urls.py
+++ Proyecto/turnos/urls.py
@@ -5,4 +5,8 @@ app_name = 'turnos'
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
+    path('crear_disponibilidad/', views.crear_disponibilidad, name='crear_disponibilidad'),
+    path('ver_disponibilidad/', views.ver_disponibilidad, name='ver_disponibilidad'),
+    path('editar/<int:id_disponibilidad>/', views.editar_disponibilidad, name='editar_disponibilidad'),
+    path('borrar/<int:id_disponibilidad>/', views.borrar_disponibilidad, name='borrar_disponibilidad'),
 ]
